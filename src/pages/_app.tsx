e99eed5fb441f274c6f9e5902df00844d8276a4e
Howard Tai <howardtai86@gmail.com>
2021-01-08 15:53:11 +0800

    feat: add ChakraProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import React, { FC } from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
-import '@/styles/globals.css';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+  <ChakraProvider>
+    <Component {...pageProps} />
+  </ChakraProvider>
 );
 
 export default App;
