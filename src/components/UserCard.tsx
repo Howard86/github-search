0f3773671d2f4a84c2e1df79e4cec8a59c889b86
Howard Tai <howardtai86@gmail.com>
2021-01-22 00:24:43 +0800

    fix: update UserCard styles

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,15 +1,20 @@
 import React, { FC } from 'react';
-import { Avatar, Text, WrapItem } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import { Avatar, Link, Text, WrapItem } from '@chakra-ui/react';
 import type { GitHubUser } from '@/server/user';
 
 type UserCardProps = GitHubUser;
 
 const UserCard: FC<UserCardProps> = ({ avatarUrl, username }) => (
-  <WrapItem alignItems="center" m={[2, 4]} maxW="36">
-    <Avatar name={username} src={avatarUrl} />
-    <Text mx="2" isTruncated>
-      {username}
-    </Text>
+  <WrapItem m={[2, 4]}>
+    <NextLink href={`/user/${username}`} passHref>
+      <Link textAlign="center" maxW={[28, 36, 44]}>
+        <Avatar name={username} src={avatarUrl} size="xl" />
+        <Text mx="2" isTruncated>
+          {username}
+        </Text>
+      </Link>
+    </NextLink>
   </WrapItem>
 );
 
