272994dca1bbc694b0176eb9e85ea0ee5d62a5f7
Howard Tai <howardtai86@gmail.com>
2021-01-24 23:50:09 +0800

    feat: update user page with Profile & Tabs

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import { Container, Flex } from '@chakra-ui/react';
 import ColorModeSwitch from './ColorModeSwitch';
 
 const Layout: FC = ({ children }) => (
-  <Container as="main" p={[4, 6, 8]} maxW="fill" minH="80vh" centerContent>
+  <Container as="main" p={[2, 6, 8]} maxW="fill" minH="80vh" centerContent>
     <Flex mb={[2, 4]} w="full" direction="row-reverse">
       <ColorModeSwitch />
     </Flex>
