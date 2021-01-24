5b6390240d4f24798dc484d1a58b5b05990a6ddb
Howard Tai <howardtai86@gmail.com>
2021-01-24 23:45:09 +0800

    feat: add InfoList

---
+++ src/components/InfoList.tsx
@@ -0,0 +1,27 @@
+import React, { FC, memo } from 'react';
+import NextLink from 'next/link';
+import { Link, ListIcon, ListItem } from '@chakra-ui/react';
+import type { IconType } from 'react-icons';
+
+interface InfoList {
+  name: string;
+  url?: string;
+  icon?: IconType;
+}
+
+const InfoList: FC<InfoList> = ({ name, icon, url }) => (
+  <ListItem>
+    <ListIcon as={icon} />
+    {url ? (
+      <Link href={url} isExternal>
+        {name}
+      </Link>
+    ) : (
+      <NextLink href={`/user/${name}`} passHref>
+        <Link>{name}</Link>
+      </NextLink>
+    )}
+  </ListItem>
+);
+
+export default memo(InfoList);
