3e9df1a7bbdc0ba61ed5d6420b0c249e05cc2c86
Howard Tai <howardtai86@gmail.com>
2021-01-25 00:13:04 +0800

    feat: add page transition in Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,45 @@
 import React, { FC } from 'react';
-import { Container, Flex } from '@chakra-ui/react';
+import { Container, Flex, Link } from '@chakra-ui/react';
+import { motion, Variants } from 'framer-motion';
+import { useRouter } from 'next/router';
 import ColorModeSwitch from './ColorModeSwitch';
+import NextLink from 'next/link';
 
-const Layout: FC = ({ children }) => (
-  <Container as="main" p={[2, 6, 8]} maxW="fill" minH="80vh" centerContent>
-    <Flex mb={[2, 4]} w="full" direction="row-reverse">
-      <ColorModeSwitch />
-    </Flex>
-    {children}
-  </Container>
-);
+const variants: Variants = {
+  pageInitial: {
+    opacity: 0,
+  },
+  pageAnimate: {
+    opacity: 1,
+  },
+};
+
+const Layout: FC = ({ children }) => {
+  const router = useRouter();
+
+  return (
+    <Container
+      as={motion.main}
+      p={[2, 6, 8]}
+      maxW="fill"
+      minH="80vh"
+      initial="pageInitial"
+      animate="pageAnimate"
+      variants={variants}
+      key={router.asPath}
+      centerContent
+    >
+      <Flex mb={[2, 4]} w="full" justify="space-between">
+        <NextLink href="/" passHref>
+          <Link fontSize="lg" fontWeight="bold">
+            Home
+          </Link>
+        </NextLink>
+        <ColorModeSwitch />
+      </Flex>
+      {children}
+    </Container>
+  );
+};
 
 export default Layout;
