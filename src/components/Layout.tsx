af2f71da8a8b7be255ec116b0aa85f7b00d0fd6a
Howard Tai <howardtai86@gmail.com>
2021-01-29 12:34:51 +0800

    feat: add NProgress bar and start after a page transition

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,8 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import NextLink from 'next/link';
 import { useRouter } from 'next/router';
 import { motion, Variants } from 'framer-motion';
+import NProgress from 'nprogress';
 import { FaHome } from 'react-icons/fa';
 import { Container, Flex, Icon, Link } from '@chakra-ui/react';
 import ColorModeSwitch from './ColorModeSwitch';
@@ -18,6 +19,26 @@ const variants: Variants = {
 const Layout: FC = ({ children }) => {
   const router = useRouter();
   const key = router.asPath.includes('?') ? router.pathname : router.asPath;
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      NProgress.start();
+    };
+
+    const handleRouterFinish = () => {
+      NProgress.done();
+    };
+
+    router.events.on('routeChangeStart', handleRouteChange);
+    router.events.on('routeChangeComplete', handleRouterFinish);
+    router.events.on('routeChangeError', handleRouterFinish);
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange);
+      router.events.off('routeChangeComplete', handleRouterFinish);
+      router.events.off('routeChangeError', handleRouterFinish);
+    };
+  }, []);
 
   return (
     <Container
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
 import type { NextPage } from 'next';
+import Head from 'next/head';
 import { DefaultSeo } from 'next-seo';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Provider as ReduxProvider } from 'react-redux';
+
 import Layout from '@/components/Layout';
 import DEFAULT_SEO from '@/constants/seo';
 import store from '@/redux/store';
@@ -11,6 +13,9 @@ import theme from '@/styles/theme';
 
 const App: NextPage<AppProps> = ({ Component, pageProps }) => (
   <>
+    <Head>
+      <link rel="stylesheet" type="text/css" href="/nprogress.css" />
+    </Head>
     <DefaultSeo {...DEFAULT_SEO} />
     <ReduxProvider store={store}>
       <ChakraProvider theme={theme}>
